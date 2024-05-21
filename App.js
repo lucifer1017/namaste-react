3786c649f125e3a597cd2a59a9536e2623a60522
lucifer1017 <lucifergrey1017@gmail.com>
2024-05-21 21:39:15 +0530

    Day 2 Learnings

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child2" },
-    [React.createElement("h1", { id: "head" }, "hey there"), React.createElement("h2", { id: "head" }, "hello")]),
+    [React.createElement("h1", { id: "head" }, "hey there1"), React.createElement("h2", { id: "head" }, "hello")]),
 React.createElement("div", { id: "child1" },
-    [React.createElement("h1", { id: "head" }, "hey there"), React.createElement("h2", { id: "head" }, "hello")])]);
+    [React.createElement("h1", { id: "head" }, "hey thereyo"), React.createElement("h2", { id: "head" }, "hello")])]);
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-#Learning React from Scratch
+# Learning React from Scratch
+# Parcel
+- Dev Build
+- Local Server
+- HMR= Hot Module Replacement
+- File Watching Algorithm= written in C++
+- Uses Caching= provides faster builds
+- Image optimization
+- Minification of files for Production
+- Bundling our files for Production
+- Compress our files for Production
+- Content Hashing
+- Code Splitting
+- Differential Bundling= to give support to be run in older browsers
+- Error handling and suggestions to fix them
+- Provides a way to host on HTTPS
+- Tree Shaking- remove unused code for you
